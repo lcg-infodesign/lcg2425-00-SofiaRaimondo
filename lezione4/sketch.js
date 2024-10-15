1c18add65d332a486756fc231c4836347a53e887
SofiaRaimondo <sofiaraimondo2@gmail.com>
2024-10-15 10:16:16 +0200

    aggiunto lezione 4

---
+++ lezione4/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+  rectMode(CENTER)
+}
+
+function draw() {
+  background(220);
+  strokeWeight(1);
+  fill(orange);
+  translate(100, 100);
+  rect(50, 50, 100, 50);
+  //-> 5
+  strokeWeight(5);
+  point(50,50);
+  
+
+}
